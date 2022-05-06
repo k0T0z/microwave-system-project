923fc7dad1f6967f63655dbf2934e36b016de6a1
saifkandil <saifsalaheldeen2002@gmail.com>
2022-05-06 21:58:10 +0200

    last init

--- popcorn.c
+++ popcorn.c
@@ -1,12 +1,14 @@
 #include "tm4c123gh6pm.h"
 
-void LCDString(char[]);
+void lcdString(char[]);
 
 void popcorn() {
-	LCDString("POPCORN"); //for printing "popcorn" on the LCD
-	
-	
-	
+	// here we should change E (enable of LCD) from 0 to 1
+	// here we should make delay
+	lcdString("POPCORN"); // for sending data to LCD
+	// here we should change E (enable of LCD) back to zero
+	// here we should start a timer in a specific area on LCD of 1 minute
+	// may be we need to always check for conditions while cooking
 	
 }
 
