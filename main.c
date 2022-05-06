0671702767a3bae8401565af1722a27a71eece45
saifkandil <saifsalaheldeen2002@gmail.com>
2022-05-06 22:29:27 +0200

    add Systick function for delays

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "stdint.h"
-#include "popcorn.c"
+#include "popcorn.c" // for popcorn function
+#include "consts.c" // for constants file
+
 void SystemInit() {} // needed in startup code file
 	
 
@@ -7,9 +9,15 @@ unsigned char keypadFetch(void);
 unsigned char sw1Fetch(void);
 unsigned char sw2Fetch(void);
 unsigned char sw3Fetch(void);
+void wait(unsigned int);
+void wait10ms(unsigned int);
+void timerInit(void);
 
+
+
+	
 int main() {
-	unsigned char input = keypadFetch();
+	unsigned char input = keypadFetch(); // fetching pressed key from keypad
 	if(input == 'A') {
 		popcorn();
 	}
@@ -17,3 +25,25 @@ int main() {
 	
 	
 }
+
+void timerInit() { // initialisation of Systick Timer
+	NVIC_ST_CTRL_R = 0;
+	NVIC_ST_RELOAD_R = 0x00FFFFFF;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_CTRL_R = 0x00000005;
+}
+
+
+void wait10ms(unsigned int delay) { // using Systick Timer to make delay of 10ms
+	NVIC_ST_RELOAD_R = delay-1;
+	NVIC_ST_CURRENT_R = 0;
+	while((NVIC_ST_CTRL_R&0x00010000) == 0){};
+}
+
+void wait(unsigned int multiple) { // using for loop for more a bigger delay
+	unsigned int i;
+	for (i = 0 ; i < multiple ; i++) {
+		wait10ms(one10ms);
+	}
+}
+
--- popcorn.c
+++ popcorn.c
@@ -8,7 +8,8 @@ void popcorn() {
 	lcdString("POPCORN"); // for sending data to LCD
 	// here we should change E (enable of LCD) back to zero
 	// here we should start a timer in a specific area on LCD of 1 minute
-	// may be we need to always check for conditions while cooking
+	
+	// may be we need to always check for conditions while cooking if the user wants to pause cooking, cancel cooking, ...etc
 	
 }
 
