f4a909ff439806482705e41a9fbf89040a636535
Mans1611 <mans.yousef1611@gmail.com>
2022-05-14 20:58:54 +0200

    adding time out condition but still no sound, fix assigning variables

--- countdown.c
+++ countdown.c
@@ -7,13 +7,21 @@ void countdown(uint32_t time){
     unsigned char arr[5] = { '0' , '0' , ':', '0' , '0' };
     uint32_t min = 0;
     uint32_t sec = 0;
+     uint32_t temp;
     for(time; time>0;time-1000) // time decrement 1000 ms = 1s
     {
-        time/=1000;
-        sec = time % 60;
-        min = time / 60;
+
+        temp = time / 1000;
+        sec = temp % 60;
+        min = temp / 60;
         sprintf(strSec,"%d",sec); // to convert time to string 
         sprintf(strMin,"%d",min);
+
+        if(time<=0){
+            /*buzzer will beeb beeb beeb*/ 
+        }
+
+
         if(min>=0)
         {
             arr[0] = strMin[0];
