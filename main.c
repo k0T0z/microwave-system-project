823577358b8c42e58f7305beed2037c1d4413ff3
Ibraheem rizk <hawas711@gmail.com>
2022-05-25 00:13:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -127,34 +127,49 @@ int main() {
 
 
 /* ================================================================================================================================================================ */
-        case Chicken:
-		LCDcommand(Clear);
-				LCDstring("Chicken weight?");
-				delayms(2000);
-				while(1){
-					key = getchar();
-					if (key != 0)
+       case Chicken:
+			LCDcommand(Clear); // clear the screen
+			LCDcus('C'); // display custom character
+			LCDpos(0, 0); // change cursor position
+			LCDstring("Chicken weight?"); // display the intered string on LCD
+			delayms(2000); // delay 2 seconds
+			while (1)
+			{
+				key = getchar(); // key hold the value intered in the keypad , note: key in this state resembles the weight
+				if (key != 0) // if key contains data
+				{
+					temp = check_Input(key); // call function that returns 1 if wrong input is entered
+					if (temp == 1)
 					{
-						temp = check_Input(key);
-						if (temp == 1){
-							currentState = Chicken;
-							break;
-						}	
-						else{
-							while(key == 0){key = getchar();}
-							LCDcommand(Clear);
-							LCDdata(key);
-							key -= 48;
-							delayms(2000);
-							time = 12 * key;
-							if(get_SW3()){
-								timer(time);
-								break;
-							}
+						currentState = Chicken; // return to beginning of Chicken state
+						break;
+					}
+					else // valid input is entered 
+					{
+						while (key == 0) // key conatins no data
+						{
+							key = getchar(); // key hold the value intered in the keypad
 						}
+						LCDcommand(Clear); // clear the screen
+						LCDdata(key); // display the data in key on LCD
+						key -= 48; // data changed to integer
+						delayms(2000); // delay 2 seconds
+						time = 12 * key; // cooks for weight * 0.5 minutes 
+						if(!get_SW3()){ // if door is opened don't change state to cooking
+						while(!get_SW3()){ // while the door is opened
+						delayms(20); // delay to be able to check sw3 again
+						if(get_SW3()) // if door is closed change state to cooking
+						break;
+							}
+							currentState = Cooking;
+							break;
+						}
+						currentState = Cooking;
+						break;
 					}
 				}
-				break;
+			}
+			break;
 			
 
 /* ================================================================================================================================================================ */
